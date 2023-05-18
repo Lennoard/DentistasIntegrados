5a3155212a439391d5d6b28bde21f9835c809c5a
Lennoard <lennoardrai@gmail.com>
2023-05-18 00:34:55 -0300

    sign up shell

--- src/presentation/pages/SignUp/index.tsx
+++ src/presentation/pages/SignUp/index.tsx
@@ -1,27 +1,262 @@
-import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import imagerySignIn from "./imagerySignIn.png";
+import logo from "./../../images/logo.png";
+import "./signin.css";
+import {
+  Box,
+  Container,
+  Grid,
+  Snackbar,
+  SnackbarCloseReason,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 import { primaryDark } from "../../theme/pallete";
+import ToastMessage from "../../utils/ToastMessage";
+import { useNavigate } from "react-router-dom";
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+} from "@firebase/auth";
+import showLocalizedAuthError from "../../../utils/auth/AuthError";
+import container from "../../../config/inversify.config";
+import DataTypes from "../../../data/di/DataTypes";
 
-export default function SignUp(): JSX.Element {
-  
+const auth = container.get<Auth>(DataTypes.Auth);
+const HOME_ROUTE = "/home";
+
+interface SignUpData {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export default function SignIn(): JSX.Element {
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState<SignUpData>({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+  const [toast, setToast] = useState<ToastMessage>({
+    showing: false,
+    message: "Falha ao realizar cadastro",
+  });
+  const [loading, setLoading] = useState(false);
+  const [editingInPass, setEditingInPass] = useState(false);
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passRef = useRef<HTMLInputElement>(null);
+  const confirmPassRef = useRef<HTMLInputElement>(null);
+
+  const handleToastClose = (
+    _: Event | React.SyntheticEvent<any, Event>,
+    reason: SnackbarCloseReason
+  ) => {
+    if (reason === "clickaway") return;
+    setToast({ ...toast, showing: false });
+  };
+
+  const handleSignUpClick = async (
+    e: React.MouseEvent<HTMLButtonElement> | null
+  ) => {
+    e?.preventDefault();
+    const pass = credentials.password;
+    const pass2 = credentials.passwordConfirmation;
+
+    if (pass !== pass2) {
+      setToast({ showing: true, message: "As senhas não correspondem" })
+      return;
+    }
+
+    setLoading(true);
+
+    const user = await performUserCreation(credentials.email, pass);
+
+    if (user) {
+      setLoading(false);
+      navigate(HOME_ROUTE);
+    }
+  };
+
+  const performUserCreation = async (email: string, password: string) => {
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      return user;
+    } catch (error: any) {
+      setToast({
+        showing: true,
+        message: showLocalizedAuthError(error.message),
+      });
+      setLoading(false);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    if (!editingInPass) {
+      emailRef.current?.focus();
+    } else {
+      passRef.current?.focus();
+    }
+  });
+
   return (
-    <Container>
-      <Box
-        sx={{
-          height: "100vh",
-          flexDirection: "column",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Typography className="moving-text" variant="h1" color={primaryDark}>
-          Cadastro!
-        </Typography>
-        <Typography className="moving-text" variant="subtitle1">
-          Em andamento!
-        </Typography>
-      </Box>
+    <Container fixed disableGutters={true}>
+      <Grid container rowSpacing={1} columnSpacing={4}>
+        <Grid
+          id="left"
+          item
+          sm={12}
+          md={6}
+          display={{
+            md: "flex!important",
+            sm: "none!important",
+            xs: "none!important",
+          }}
+        >
+          <Box display="flex" flexDirection="row">
+            <Typography
+              marginY="16px"
+              variant="h5"
+              color={primaryDark}
+              marginRight="8px"
+            >
+              Dentistas Integrados
+            </Typography>
+            <img width="50px" height="52px" src={logo} alt="Logo" />
+          </Box>
+          <img id="app-logo" src={imagerySignIn} alt="Ilustração" />
+        </Grid>
+        <Grid
+          id="right"
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          paddingX={{ lg: "100px!important" }}
+        >
+          <Box
+            flexDirection="row"
+            display={{
+              md: "none!important",
+              sm: "flex!important",
+              xs: "flex!important",
+            }}
+          >
+            <Typography
+              marginY="16px"
+              variant="h5"
+              color={primaryDark}
+              marginRight="8px"
+            >
+              Dentistas Integrados
+            </Typography>
+            <img width="48px" height="55px" src={logo} alt="Logo" />
+          </Box>
+
+          <Typography marginTop="16px" variant="h1" textAlign="center">
+            Cadastro
+          </Typography>
+
+          <Box display="flex" flexDirection="column" marginTop={4} >
+            <TextField
+              key="name"
+              type="text"
+              label="Nome completo"
+              margin="dense"
+              inputRef={nameRef}
+              value={credentials.name}
+              onChange={(event) => {
+                setEditingInPass(false);
+                setCredentials({ ...credentials, name: event.target.value });
+              }}
+              required
+            />
+
+            <TextField
+              key="email"
+              type="email"
+              label="Email"
+              margin="dense"
+              inputRef={emailRef}
+              value={credentials.email}
+              onChange={(event) => {
+                setEditingInPass(false);
+                setCredentials({ ...credentials, email: event.target.value });
+              }}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  handleSignUpClick(null);
+                }
+              }}
+              required
+            />
+
+            <TextField
+              inputRef={passRef}
+              key="password"
+              type="password"
+              label="Senha"
+              margin="dense"
+              value={credentials.password}
+              onChange={(event) => {
+                setEditingInPass(true);
+                setCredentials({
+                  ...credentials,
+                  password: event.target.value,
+                });
+              }}
+              required
+            />
+
+            <TextField
+              inputRef={confirmPassRef}
+              key="conform-password"
+              type="password"
+              label="Confirmar senha"
+              margin="dense"
+              value={credentials.passwordConfirmation}
+              onChange={(event) => {
+                setEditingInPass(true);
+                setCredentials({
+                  ...credentials,
+                  passwordConfirmation: event.target.value,
+                });
+              }}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  handleSignUpClick(null);
+                }
+              }}
+              required
+            />
+            <LoadingButton
+              loading={loading}
+              variant="contained"
+              sx={{ minWidth: "120px", margin: "36px auto" }}
+              onClick={(e) => handleSignUpClick(e)}
+            >
+              Finalizar
+            </LoadingButton>
+
+            <Snackbar
+              open={toast.showing}
+              autoHideDuration={4000}
+              onClose={handleToastClose}
+              message={toast.message}
+            />
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
